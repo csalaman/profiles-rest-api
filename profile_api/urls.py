1c40ca88a95b7efdea30dd0de178e38112c3d761
Carlos <csalaman@terpmail.umd.edu>
2021-04-13 09:40:35 -0400

    Add ViewSet to profile_api & router

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,10 +1,16 @@
 # Define API paths for the views(aka HTTP Request/Response methods) - app_name/urls.py file created
 
 from profile_api import views
-from django.urls import path 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 # Define API paths for each view (user as_view() method)
 urlpatterns = [
+    # APIView
     path('hello-view/', views.HelloApiView.as_view()),
-    # path('', 'This is the root directory of the profile_api(../api/'),
+    # Use Router for ViewSets
+    path('', include(router.urls)),
 ]
--- profile_api/views.py
+++ profile_api/views.py
@@ -2,10 +2,14 @@
 # APIViews allows to write standard HTTP Methods as functions & give most control over the logic
 # Benefits: Perfect for implementing complex logic, calling other APIs, working with local files
 
+# Viewsets -> uses model operations for functions kist, create, retrieve, update, partial_update, destroy
+# When to use: simple CRUD interface to database, quick & simple API, little to no customization on the logic, working with standard data structures
+
 # Good to use when: need full control over the logic(complex algo, updating multiple datasources in a single API call), 
 # processing files and rendering a synchronous response, calling other APIs/services, accessing local files or data
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -57,3 +61,46 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a message"""
+        a_viewset = [
+            'Uses actions (list,create,retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+        return Response({'message':'Hello World', 'a_viewset':a_viewset})
+    
+    def create(self, request):
+        """Create a new message"""
+        # Pass to serializer & validate
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Retrieve object by ID"""
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """Update an object"""
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self,request, pk=None):
+        """Partial update on object"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Destroy an object"""
+        return Response({'http_method':'DELETE'})
